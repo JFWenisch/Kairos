4d72e84f08101993fa99ca120b181e71b914b022
Jean-Fabian Wenisch <jean-fabian@wenisch.tech>
2026-05-04 19:56:51 +0200

    feat: adding notificationpolicy

--- src/main/java/tech/wenisch/kairos/entity/NotificationProvider.java
+++ src/main/java/tech/wenisch/kairos/entity/NotificationProvider.java
@@ -1,7 +1,16 @@
 package tech.wenisch.kairos.entity;
 
-import jakarta.persistence.*;
-import lombok.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data
