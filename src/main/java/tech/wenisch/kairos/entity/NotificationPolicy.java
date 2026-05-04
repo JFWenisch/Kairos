5052d53b823a5e38930f9d9bb835ff5a3edadcfb
Jean-Fabian Wenisch <jean-fabian@wenisch.tech>
2026-05-04 19:56:51 +0200

    feat: adding notificationpolicy

--- src/main/java/tech/wenisch/kairos/entity/NotificationPolicy.java
+++ src/main/java/tech/wenisch/kairos/entity/NotificationPolicy.java
@@ -1,10 +1,24 @@
 package tech.wenisch.kairos.entity;
 
-import jakarta.persistence.*;
-import lombok.*;
-
 import java.util.HashSet;
 import java.util.Set;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data
--- src/main/java/tech/wenisch/kairos/repository/NotificationProviderRepository.java
+++ src/main/java/tech/wenisch/kairos/repository/NotificationProviderRepository.java
@@ -2,6 +2,7 @@ package tech.wenisch.kairos.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
 import tech.wenisch.kairos.entity.NotificationProvider;
 
 @Repository
